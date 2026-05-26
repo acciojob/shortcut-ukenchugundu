01c8de5e9e2493f3841a2e4601ea3afbd0fc6b47
Kenchugundu UdayKumar <134130187+ukenchugundu@users.noreply.github.com>
2026-05-26 18:08:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function shortcut(s1, s2) {
   // your code here
 	if (s1.length > 0 && s2.length > 0) {
-	return s1[0] + s2[0];
+    return s1[0] + s2[0];
   }
+
+  return "";
 }
 
 // Do not change the code below.
